e5d9eabc4c283cded966c4973dd5c4e438346cc3
Tauruswho <mark@minting.net>
2024-04-12 22:18:01 +0200

    add logrotate

--- configuration.nix
+++ configuration.nix
@@ -109,7 +109,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.gvfs.enable = true;
-
+  services.logrotate.enable = true;
   services.flatpak.enable = true;
 
   # Open ports in the firewall.
